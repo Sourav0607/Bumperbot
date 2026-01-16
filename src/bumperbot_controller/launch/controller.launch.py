44bd1be79f702c790f3c89625f16e718c2c8da47
Sourav Anil Hawaldar <sourav.hawaldar@gmail.com>
2026-01-16 13:36:29 +0100

    Added noise to robot motion

--- src/bumperbot_controller/launch/controller.launch.py
+++ src/bumperbot_controller/launch/controller.launch.py
@@ -1,8 +1,29 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node 
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
+
+def noisy_controller(context, *args, **kwargs):
+    wheel_radius = float(LaunchConfiguration('wheel_radius').perform(context))
+    wheel_separation = float(LaunchConfiguration('wheel_separation').perform(context))
+    wheel_radius_error = float(LaunchConfiguration('wheel_radius_error').perform(context))
+    wheel_separation_error = float(LaunchConfiguration('wheel_separation_error').perform(context))
+    use_python = LaunchConfiguration('use_python').perform(context)
+    
+    noisy_controller_py = Node(
+        package='bumperbot_controller',
+        executable='noisy_controller.py',
+        name='noisy_controller',
+        parameters=[{
+            'wheel_radius': wheel_radius + wheel_radius_error,
+            'wheel_separation': wheel_separation + wheel_separation_error
+        }],
+        condition = IfCondition(use_python)
+    )
+    
+    return [noisy_controller_py]
+        
 
 def generate_launch_description():
     
@@ -28,6 +49,18 @@ def generate_launch_description():
         'use_simple_controller',
         default_value='true',
         description='Whether to use the simple velocity controller'
+    )
+    
+    wheel_radius_error_arg = DeclareLaunchArgument(
+        'wheel_radius_error',
+        default_value='0.005',
+        description='Error in wheel radius'
+    )
+    
+    wheel_separation_error_arg = DeclareLaunchArgument(
+        'wheel_separation_error',
+        default_value='0.02',
+        description='Error in wheel separation'
     )
     
     use_python = LaunchConfiguration('use_python')
@@ -84,6 +117,10 @@ def generate_launch_description():
         condition = IfCondition(use_python)
     )
     
+    Noisy_controller_launch = OpaqueFunction(
+        function=noisy_controller
+    )
+    
     
     
     
@@ -91,10 +128,14 @@ def generate_launch_description():
         use_python_arg,
         wheel_radius_arg,
         wheel_separation_arg,
+        wheel_radius_error_arg,
+        wheel_separation_error_arg,
         use_simple_controller_arg,
         joint_state_broadcaster_spawnner,
         wheel_controller_spawnner,
         simple_controller_spawner,
         simple_controller_py,
+        Noisy_controller_launch,
+        
         
     ])
